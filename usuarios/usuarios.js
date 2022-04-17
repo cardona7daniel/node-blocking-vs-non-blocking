0a0bd56e9a315d0f4fd2fb477d8afaae58806e4f
luiscardona1997 <cardona7daniel@gmail.com>
2022-04-16 22:36:56 -0500

    feat: test

--- usuarios/usuarios.js
+++ usuarios/usuarios.js
@@ -1,32 +1,29 @@
+const getUsuarioSync = (id) => {
+  const startPoint = new Date().getTime();
+  while (new Date().getTime() - startPoint <= 3000) {
+    // Esperando...
+    // Haciendo fetch de base de datos...
+    // Robando datos de facebook...
+  }
 
+  return {
+    id,
+    nombre: `Usuario ${id}`,
+  };
+};
 
-const getUsuarioSync = ( id ) => {
-    const startPoint = new Date().getTime();
-    while (new Date().getTime() - startPoint <= 3000 ) {
-        // Esperando...
-        // Haciendo fetch de base de datos...
-        // Robando datos de facebook...
-    }
+const getUsuario = (id, callback) => {
+  const usuario = {
+    id,
+    nombre: `Usuario ${id}`,
+  };
 
-    return {
-        id,
-        nombre: `Usuario ${ id }`
-    }; 
-}
-
-
-const getUsuario = (id, callback ) => {
-    const usuario = {
-        id,
-        nombre: `Usuario ${ id }`
-    };
-    
-    setTimeout(() => {
-        callback( usuario );
-    }, 3000);
-}
+  setTimeout(() => {
+    callback(usuario);
+  }, 3000);
+};
 
 module.exports = {
-    getUsuario,
-    getUsuarioSync
-}
+  getUsuario,
+  getUsuarioSync,
+};
